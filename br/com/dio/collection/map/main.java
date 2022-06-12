41e55f51806e542cc28096f67cd0c6d3937fe9c0
Bryan Neves <bryan.nevesp@gmail.com>
2022-06-12 15:49:35 -0400

    mvp---project

---
+++ br/com/dio/collection/map/main.java
@@ -0,0 +1,10 @@
+package br.com.dio.collection.map;
+
+class Simple {  
+    public static void main(String args[]) {  
+
+        int br = 10;
+        int rb = 20;
+       System.out.println(br + rb);     
+    }  
+ }  
